532b2de65dcfc61d86641370e9d10dedc4178580
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2023-11-15 13:25:44 -0300

    feat: create table usuarios

--- create-tables.js
+++ create-tables.js
@@ -1,16 +1,29 @@
 import { sql } from './db.js'
 
-// sql` DROP TABLE IF EXISTS videos `.then(() => {
+// sql` DROP TABLE IF EXISTS usuarios `.then(() => {
 //   console.log('Tabela deletada')
 // })
 
-sql`
-  CREATE TABLE videos (
-    id          TEXT PRIMARY KEY,
-    title       TEXT,
-    description TEXT,
-    duration    INTEGER
-  );
-`.then(() => {
-  console.log('Tabela criada')
-})
+// sql`
+//   CREATE TABLE videos (
+//     id          TEXT PRIMARY KEY,
+//     title       TEXT,
+//     description TEXT,
+//     duration    INTEGER
+//   );
+// `.then(() => {
+//   console.log('Tabela criada')
+// })
+
+// sql`
+//   CREATE TABLE usuarios (
+//     id                  SERIAL PRIMARY KEY,
+//     name                VARCHAR(255),
+//     email               VARCHAR(255) UNIQUE,
+//     senha               VARCHAR(255),
+//     numero_matricula    VARCHAR(20),
+//     telefone            VARCHAR(15)
+//   );
+// `.then(() => {
+//   console.log('Tabela usuarios criada')
+// })
